95f23bc03c3416ef877519bfdefd939e83efb144
ChallenAi <fengyiai@gmail.com>
2025-04-02 14:50:56 +0000

    feat: add a generateRandomString function, format code

--- utils.ts
+++ utils.ts
@@ -1,25 +1,26 @@
 import { MD5 } from "bun";
+import crypto from "node:crypto";
 
-export const caculateHashID = (info: string): string => {
+export function caculateHashID(info: string): string {
     const h = MD5.hash(info).toString();
     return Buffer.from(h).toString("hex");
-};
+}
 
-export const getNameFromPath = (path: string): string => {
+export function getNameFromPath(path: string): string {
     for (let i = path.length - 1; i >= 0; i--) {
         if (path.charAt(i) === "/") return path.substring(i + 1);
     }
     return path;
-};
+}
 
-export const getPrefixPathFromPath = (path: string): string => {
+export function getPrefixPathFromPath(path: string): string {
     for (let i = path.length - 1; i >= 0; i--) {
         if (path.charAt(i) === "/") return path.substring(0, i);
     }
     return "";
-};
+}
 
-export const normalizePath = (path: string): string => {
+export function normalizePath(path: string): string {
     let path_ = path.trim();
     if (!path_.startsWith("/")) path_ = `/${path}`;
     for (let n = path_.length - 1; n >= 0; n--) {
@@ -27,4 +28,8 @@ export const normalizePath = (path: string): string => {
         if (ch !== "/" && ch !== ".") return path_.slice(0, n + 1);
     }
     return "/";
-};
+}
+
+export function generateRandomString(length = 8): string {
+    return `${crypto.randomBytes(length).toString("hex").slice(0, length)}`;
+}
